43512032359be614ffc53a8261981c295bd00451
HONORE74 <komboigohonore823@gmail.com>
2026-09-16 12:52:17 +0200

    Clean up honoc.py by removing unused code
    Removed commented-out code for printing column names and feature importances.

--- honoc.py
+++ honoc.py
@@ -1,9 +1,1 @@
-print([c for c in df.columns if "_lag_" in c])
 
-
-# Après entraînement :
-importance = pd.Series(
-    modele_apres.feature_importances_,
-    index=modele_apres.feature_name_
-).sort_values(ascending=False)
-print(importance.head(10))
