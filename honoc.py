c42b8cf8425f12facfc63734692d022f296fff28
HONORE74 <komboigohonore823@gmail.com>
2026-09-18 15:35:35 +0200

    Refactor app to use Dash framework

--- honoc.py
+++ honoc.py
@@ -1,1 +1,4 @@
-        app.run(debug=False, jupyter_mode="inline", port=8050)
+from dash import Dash, html
+app = Dash(__name__)
+app.layout = html.Div("Ca marche.")
+app.run(jupyter_mode="inline", port=8060)
