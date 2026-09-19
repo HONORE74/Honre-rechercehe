79e13ec297efd2ed83c46ae322104be3c646ddb4
HONORE74 <komboigohonore823@gmail.com>
2026-09-20 01:12:42 +0200

    Refactor model references to use modele_apres
    Updated model references and prediction variables to use 'modele_apres' instead of 'modele_final'. Adjusted true and predicted value assignments accordingly.

--- honoc.py
+++ honoc.py
@@ -1,1 +1,86 @@
-comparaison_test = perf_avant_split.merge(perf_apres_split, on="Split", suffixes=("_avant", "_apres"))
+# Remplace :
+y_pred_te = modele.predict(X_te)
+# Par :
+y_pred_te = modele_apres.predict(X_te)
+
+
+
+
+
+
+
+
+# Remplace :
+last_model = modele_final
+# Par :
+last_model = modele_apres
+
+
+
+
+
+
+
+
+
+# Remplace :
+model_lgbm = modele_final
+# Par :
+model_lgbm = modele_apres
+
+
+
+
+
+
+
+
+
+
+
+
+# Remplace :
+last_model = modele_final
+# Par :
+last_model = modele_apres
+
+
+
+
+
+
+
+
+
+
+# Remplace :
+y_true = df_perf["Valeur_Reelle"].values.astype(float)
+y_pred = df_perf["Valeur_Predite"].values.astype(float)
+# Par :
+y_true = df_perf["y_obs"].values.astype(float)
+y_pred = df_perf["y_pred"].values.astype(float)
+
+
+
+
+
+
+
+
+
+
+
+
+# Remplace :
+model=modele_final
+# Par :
+model=modele_apres
+
+
+
+
+
+
+
+
+
